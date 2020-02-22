3c5a2f0e5222c77469c9356f5bd5a983c3aa053a
naterpotatoers <nate.garza@hotmail.com>
2020-02-22 14:40:40 -0800

    Andrews fixed encoder code, still return in degree

--- library/L3_Application/Robotics/Common/MagneticEncoder.hpp
+++ library/L3_Application/Robotics/Common/MagneticEncoder.hpp
@@ -1,10 +1,9 @@
 #pragma once
-
 #include <cstdint>
 #include "utility/log.hpp"
 #include "L1_Peripheral/i2c.hpp"
-
-// Change these address values later
+#include "L1_Peripheral/lpc40xx/i2c.hpp"
+#include "utility/map.hpp"
 
 namespace sjsu
 {
@@ -14,41 +13,40 @@ class MagneticEncoder
 {
  public:
   static constexpr uint8_t kMagneticEncoderAddress   = 0x36;
-  static constexpr uint8_t kRawAngleHighRegister     = 0x0E;
-  static constexpr uint8_t kRawAngleLowRegister      = 0x0F;
-  static constexpr uint8_t kMsbShift                 = 8;
+  static constexpr uint8_t kMagneticEncoderMagnitude = 0x1B;
+  static constexpr uint8_t kEncoderAngle             = 0x0E;
   static constexpr units::angle::degree_t kZeroAngle = 0_deg;
+
   explicit constexpr MagneticEncoder(sjsu::I2c & i2c)
       : current_angle_(kZeroAngle), i2c_(i2c)
   {
   }
+
   void Initialize()
   {
     i2c_.Initialize();
-    // idk what goes here figure the address out later
-    i2c_.Write(kMagneticEncoderAddress, { 0x1 });
   }
 
   units::angle::degree_t GetAngle() const
   {
-    // i have no idea how to read with i2c, gotta read datasheet here
-    int tilt_reading;
-    int16_t axis_tilt;
-    LOG_INFO("before: %d", axis_tilt);
-    uint8_t tilt_val[2];
+    uint8_t register_vals[2];
+    uint16_t angle;
+    uint16_t mapedAngle;
+
     i2c_.WriteThenRead(kMagneticEncoderAddress,
-                       { kRawAngleLowRegister },
-                       tilt_val,
-                       sizeof(tilt_val));
-    tilt_reading = (tilt_val[0] << kMsbShift) | tilt_val[1];
-    axis_tilt    = static_cast<int16_t>(tilt_reading);
-    LOG_INFO("after: %d %d", axis_tilt, tilt_val[1]);
-    units::angle::degree_t angle(axis_tilt);
-    return angle;
+                       { kEncoderAngle },
+                       register_vals,
+                       sizeof(register_vals));
+
+    angle      = register_vals[0] << 8 | register_vals[1];
+    mapedAngle = sjsu::Map(angle, 0, 4090, 0, 360);
+
+    LOG_INFO("\nAngle: %d\nMaped: %d\n", angle, mapedAngle);
+    // return (units::angle::deg)mapedAngle; //How to convert to degrees??
   }
+
   void SetZero()
   {
-    // do some i2c here to set the current angle to zero.
     current_angle_ = kZeroAngle;
   }
 
